b3bf8f123bfe8a6cb5234d51e18b73db81a63508
Ahad69 <ahadxx99@gmail.com>
2023-07-17 00:56:29 +0600

    remove all wishlist and page loader and empty warning added

--- src/pages/Wishlist.tsx
+++ src/pages/Wishlist.tsx
@@ -4,12 +4,12 @@ import { useAppSelector } from "../redux/hook";
 import style from "./../pages-css/wishlist.module.css";
 import { MyBook } from "../Interfaces/globalTypes";
 import { removeFromWishList } from "../redux/features/wishlistSlice";
-import { message } from "antd";
+import { Tooltip, message } from "antd";
 import { useDispatch } from "react-redux";
 import { persistor } from "../redux/store";
 import { isLoading } from "../redux/features/commonOptionsSlice";
 import Loader from "../components/ui/loader";
-import { BsFillHeartbreakFill } from "react-icons/bs";
+import { BsFillHeartbreakFill, BsFillTrash3Fill } from "react-icons/bs";
 
 export default function Wishlist() {
   const { wishlist } = useAppSelector((state) => state.wishlist);
@@ -28,9 +28,7 @@ export default function Wishlist() {
   const handleClearStore = () => {
     dispatch(isLoading(true));
     persistor.purge();
-    setTimeout(() => {
-      dispatch(isLoading(false));
-    }, 1000);
+    window.location.reload();
   };
 
   return (
@@ -48,25 +46,38 @@ export default function Wishlist() {
               <BsFillHeartbreakFill className="text-6xl text-red-700 block m-auto mt-5 " />
             </div>
           ) : (
-            <div className={style.wishlistContainer}>
-              {wishlist.map((a) => (
-                <div className="w-full h-32 bg-gray-50 border border-white hover:shadow-md rounded hover:shadow-blue-500/50 p-2 flex justify-around">
-                  <img className="w-24 h-28" src={a.image} />
-                  <div className="flex flex-col w-96">
-                    <p className="text-xl font-bold">{a.title}</p>
-                    <p className="font-bold uppercase text-blue-400">
-                      {a.genre}
-                    </p>
-                    <p className="font-bold text-xl">{a.publicationDate}</p>
+            <>
+              <Tooltip
+                placement="topRight"
+                title={"Remove All"}
+                arrow={true}
+                color={"blue"}
+              >
+                <BsFillTrash3Fill
+                  onClick={() => handleClearStore()}
+                  className="text-xl block ml-auto my-5 text-red-500 cursor-pointer"
+                />
+              </Tooltip>
+              <div className={style.wishlistContainer}>
+                {wishlist.map((a) => (
+                  <div className="w-full h-32 bg-gray-50 border border-white hover:shadow-md rounded hover:shadow-blue-500/50 p-2 flex justify-around">
+                    <img className="w-24 h-28" src={a.image} />
+                    <div className="flex flex-col w-96">
+                      <p className="text-xl font-bold">{a.title}</p>
+                      <p className="font-bold uppercase text-blue-400">
+                        {a.genre}
+                      </p>
+                      <p className="font-bold text-xl">{a.publicationDate}</p>
 
-                    <AiFillHeart
-                      onClick={() => handleRemoveFromCart(a)}
-                      className="text-2xl text-red-500 cursor-pointer"
-                    />
+                      <AiFillHeart
+                        onClick={() => handleRemoveFromCart(a)}
+                        className="text-2xl text-red-500 cursor-pointer"
+                      />
+                    </div>
                   </div>
-                </div>
-              ))}
-            </div>
+                ))}
+              </div>{" "}
+            </>
           )}
         </>
       )}
